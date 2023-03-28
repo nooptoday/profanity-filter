ceb84d524f0c4cfd519d959c3da3d90ca6d21197
muhammedtan <muhammedkadirtan@gmail.com>
2023-03-28 17:20:47 +0300

    style(lint)

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,12 @@
 use napi_derive::napi;
 use rustrict::{Censor, CensorStr, Type};
 
-
 #[napi]
 pub fn plus_100(input: u32) -> u32 {
-    input + 100
+  input + 100
 }
 
 #[napi]
 pub fn censor(input: String) -> String {
-    input.censor()
+  input.censor()
 }
